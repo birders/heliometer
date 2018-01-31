f38894c13872652cdffc9ee65d2cc41111d31e70
LEARAX57H <willculhane3@gmail.com>
2018-01-30 22:20:56 -0700

    Minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -86,12 +86,12 @@ pub fn execute<R: std::io::Read, W: std::io::Write>(source: &str, input: R, outp
 
 
 /*
->	increment the data pointer (to point to the next cell to the right).
-<	decrement the data pointer (to point to the next cell to the left).
-+	increment (increase by one) the byte at the data pointer.
--	decrement (decrease by one) the byte at the data pointer.
-.	output the byte at the data pointer.
-,	accept one byte of input, storing its value in the byte at the data pointer.
-[	if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
-]	if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
+  [ ]  >	increment the data pointer (to point to the next cell to the right).
+  [ ]  <	decrement the data pointer (to point to the next cell to the left).
+  [x]  +	increment (increase by one) the byte at the data pointer.
+  [ ]  -	decrement (decrease by one) the byte at the data pointer.
+  [x]  .	output the byte at the data pointer.
+  [ ]  ,	accept one byte of input, storing its value in the byte at the data pointer.
+  [ ]  [	if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+  [ ]  ]	if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
 */
