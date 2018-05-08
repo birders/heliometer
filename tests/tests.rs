0733f2659e4c1c67db7f8bd2d9f5744c7c565b1f
LEARAX57H <willculhane3@gmail.com>
2018-05-07 22:35:46 -0600

    Basic test for size of tape

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,5 @@
 extern crate assert_cli;
+extern crate heliometer;
 
 static INVALID_USAGE_OUTPUT_1: &'static str = "Usage: ";
 static INVALID_USAGE_OUTPUT_2: &'static str = " <file.bf>";
@@ -19,5 +20,25 @@ mod integration {
             .stderr()
             .contains(INVALID_USAGE_OUTPUT_2)
             .unwrap();
+    }
+}
+
+mod tests {
+    use heliometer;
+    use std;
+    use std::io::Read;
+
+    #[test]
+    fn far_right() {
+
+        let mut file = std::fs::File::open("examples/far_right.bf").unwrap();
+        let mut input = String::new();
+        file.read_to_string(&mut input).unwrap();
+
+        let mut buff = std::io::Cursor::new(vec![0; 1]);
+
+        heliometer::execute(&input, &mut std::io::stdin(), &mut buff).unwrap();
+
+        assert_eq!(&buff.get_ref()[0], &b'#');
     }
 }
