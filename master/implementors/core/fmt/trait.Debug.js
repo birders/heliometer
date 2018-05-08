d54f8092dfd404440cc22e91ec5138b716378d59
Travis CI User <travis@example.org>
2018-05-08 05:02:43 +0000

    Automatic Travis documentation build

--- master/implementors/core/fmt/trait.Debug.js
+++ master/implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["heliometer"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"heliometer/enum.Error.html\" title=\"enum heliometer::Error\">Error</a>",];
+implementors["heliometer"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"heliometer/enum.Error.html\" title=\"enum heliometer::Error\">Error</a>",synthetic:false,types:["heliometer::Error"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
