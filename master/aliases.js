7b50ff8d6b9b64e56642da9dae5497f6d3b46ebe
Travis CI User <travis@example.org>
2018-05-08 02:48:31 +0000

    Automatic Travis documentation build

---
+++ master/aliases.js
@@ -0,0 +1,9 @@
+var ALIASES = {};
+ALIASES['helio'] = {};
+ALIASES['helio'] = {};
+ALIASES['helio'] = {};
+ALIASES['helio'] = {};
+ALIASES['heliometer'] = {};
+ALIASES['heliometer'] = {};
+ALIASES['heliometer'] = {};
+ALIASES['heliometer'] = {};
